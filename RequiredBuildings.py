826825820e29aaa7aa57bbdbc7a347150ffadc09
asgeirorns <asgeirorns@gmail.com>
2016-11-03 12:16:09 +0000

    Add files via upload

--- RequiredBuildings.py
+++ RequiredBuildings.py
@@ -25,28 +25,46 @@ with open('bordarodun2.csv',"r",encoding='latin-1', newline='') as csvfile:
         for i in range(10):
             DI[i][rows[2]] = rows[i+1]
 
+            #/*
+            #ID number for buildings
+            #Adalbygging	1
+            #Askja	2
+            #Arnagardur	3
+            #Eirberg	4
+            #Gimli	5
+            #Haskolatorg	6
+            #Logberg	7
+            #Oddi	8
+            #VRII	9
+            #Hamar	10
+            #Klettur	11
+            #Enni	12
+            #Laugarvatn	13
+            #Nýji Garður 14
+            #*/
+
 
 with open('RequiredBuildings.dat','w', encoding=_wenc) as fdat:
     for course, blist in DI[0].items():
         s = 'set RequiredBuildings['+course.translate(_trans)+']:='
         fdat.write(s)
         if blist =='11' or blist=='12' or blist=='13' or blist=='14' or blist=='15' or blist=='16':
-            fdat.write('6 8')
+            fdat.write('Haskolatorg Oddi Logberg Arnagardur ')
             fdat.write(';')
         if blist=='21':
-            fdat.write('4 1')
+            fdat.write('Eirberg Adalbygging')
             fdat.write(';')
         if blist=='22' or blist=='23' or blist=='24' or blist=='25' or blist=='26':
-            fdat.write('9 4 1 6 8')
+            fdat.write('VRII Eirberg Adalbygging Haskolatorg Oddi Arnagardur Oddi')
             fdat.write(';')
         if blist =='31' or blist=='32' or blist=='33' or blist=='34':
-            fdat.write('1 3 6 7')
+            fdat.write('Adalbygging Arnagardur Haskolatorg Logberg Oddi')
             fdat.write(';')
         if blist =='41' or blist=='42' or blist=='43':
-            fdat.write('10 11 12')
+            fdat.write('Hamar Klettur Enni')
             fdat.write(';')
         if blist =='51' or blist =='52' or blist =='53' or blist =='54' or blist=='55' or blist=='56':
-            fdat.write('9 2 3 6')
+            fdat.write('VRII Askja Arnagardur Haskolatorg Oddi Adalbygging Eirberg')
             fdat.write(';')
 
         fdat.write('\n')
