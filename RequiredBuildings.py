1393bcfe7e408321792946553a7cc58539c43f03
Thomas Philip Runarsson <tprunarsson@users.noreply.github.com>
2016-11-09 11:37:41 +0000

    Update RequiredBuildings.py

--- RequiredBuildings.py
+++ RequiredBuildings.py
@@ -9,7 +9,6 @@
 #The document looks like:
 #Fræðasvið	Deild	Stuttfagnúmer	Fagheiti	Dagsetning	Staðnemar	Heildarfjöldi	Tegund lokaprófs
 
-
 import os, csv
 from collections import OrderedDict
 _trans = str.maketrans('ÁÐÉÍÓÚÝÞÆÖáðéíóúýþæö_ ','ADEIOUYTAOadeiouytao_ ')
@@ -25,25 +24,6 @@ with open('bordarodun2.csv',"r",encoding='latin-1', newline='') as csvfile:
     for rows in RoomData:
         for i in range(6):
             DI[i][rows[2]] = rows[i+1]
-
-            #/*
-            #ID number for buildings
-            #Adalbygging	1
-            #Askja	2
-            #Arnagardur	3
-            #Eirberg	4
-            #Gimli	5
-            #Haskolatorg	6
-            #Logberg	7
-            #Oddi	8
-            #VRII	9
-            #Hamar	10
-            #Klettur	11
-            #Enni	12
-            #Laugarvatn	13
-            #Nýji Garður 14
-            #*/
-
 
 with open('RequiredBuildings.dat','w', encoding=_wenc) as fdat:
     for course, blist in DI[0].items():
@@ -75,7 +55,7 @@ with open('RequiredBuildings.dat','w', encoding=_wenc) as fdat:
             fdat.write('Oddi Hamar Haskolatorg Arnagardur')
             fdat.write(';')
         if blist=='26':
-            fdat.write('Eirberg Torfan')
+            fdat.write('Eirberg Logberg Oddi Haskolatorg Gimli Arnagardur')
             fdat.write(';')
         if blist =='31' or blist=='32' or blist=='33' or blist=='34':
             fdat.write('Arnagardur Adalbygging Haskolatorg Logberg Oddi')
